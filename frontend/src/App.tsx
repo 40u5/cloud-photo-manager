910fe0b7d029e7fd64cea5ee75129a10e3498243
40u5 <meswag74@gmail.com>
2025-08-13 17:31:06 -0400

    fixed bad import in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { Provider, AddProviderRequest, RemoveProviderRequest, AddProviderResponse, StatusType } from './types';
 import ProviderItem from './components/ProviderItem';
 import StatusAlert from './components/StatusAlert';
